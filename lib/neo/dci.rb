a75c57ccd708d978498dcdbc79522d6696e18796
Peter Suschlik <ps@neopoly.de>
2012-12-11 18:04:46 +0100

    Inject context result class.

--- lib/neo/dci.rb
+++ lib/neo/dci.rb
@@ -1,7 +1,12 @@
+module Neo
+  module DCI
+  end
+end
+ 
 require 'neo/dci/version'
-
 require 'neo/dci/data'
 require 'neo/dci/role'
 require 'neo/dci/context'
+require 'neo/dci/context_result'
 
 require 'neo/dci/task_loader' if defined?(::Rails::Railtie)
